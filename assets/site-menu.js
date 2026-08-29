53f5a720b343cc9b0a04150b240cb455b8b8f78a
phodara <phodara@gmail.com>
2026-08-29 07:45:46 -0400

    added pocket prowler to website

--- assets/site-menu.js
+++ assets/site-menu.js
@@ -7,8 +7,8 @@
     { href: 'index.html', label: 'Vidiotbox-website' },
     { href: 'pictograms.html', label: 'Pictograms' },
     { href: 'vidiotbox-hardware_docs.html', label: 'Vidiotbox-hardware Docs' },
-    { href: 'lilygo-t-display-c5.html', label: 'LilyGO T-Display C5 WiFi/BLE Analyzer' },
-    { href: 'docs/pocketprowler-instruction-manual.html', label: 'Pocket Prowler Instruction Manual' },
+    { href: 'lilygo-t-display-c5.html', label: 'POCKET PROWLER WIFI/BLE ANALYZER' },
+    { href: 'docs/pocketprowler-instruction-manual.html', label: 'POCKET PROWLER MANUAL' },
     {
       type: 'section',
       label: 'Paul Hodara'
